d46bce16ba526480c6c025b54f5fa241c3e1ac95
sofiacharnota <sofiacharnota@gmail.com>
2023-10-12 17:41:18 +0200

    add adamw optim to get rid of warning

--- train-torchrun.py
+++ train-torchrun.py
@@ -101,7 +101,7 @@ class ModelTrainer:
 
         trainer = Trainer(model=self.pretrained_model, args=trainer_args, tokenizer=self.tokenizer,
                           data_collator=seq2seq_data_collator, train_dataset=dataset_samsum_pt,
-                          eval_dataset=eval_dataset)
+                          eval_dataset=eval_dataset, optimizers=(AdamW))
 
         trainer.train()
 
